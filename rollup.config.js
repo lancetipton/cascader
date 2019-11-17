428265a66db0217001d4da90e86e717f6e78e2af
lancetipton <lancetipton04@gmail.com>
2019-11-16 22:04:27 -0700

    Updates   * Added buld file   * Updated rollup config for production builds   * Updated plugin-replace dep

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,9 @@
-import { rollup } from 'rollup'
 import babel from 'rollup-plugin-babel'
 import resolve from 'rollup-plugin-node-resolve'
-import replace from 'rollup-plugin-replace'
+import replace from '@rollup/plugin-replace'
 import commonjs from 'rollup-plugin-commonjs'
 import cleanup from 'rollup-plugin-cleanup'
+import sourcemaps from 'rollup-plugin-sourcemaps';
 
 const outputFile = "./build/index.js"
 
@@ -13,14 +13,19 @@ export default {
     file: outputFile,
     format: "cjs"
   },
-  external: id => /^react/.test(id),
+  watch: {
+    clearScreen: false
+  },
+  external: ['react', 'jsutils' ],
   plugins: [
     replace({
       "process.env.NODE_ENV": JSON.stringify('production')
     }),
     babel({
-      exclude: "node_modules/**"
+      exclude: "node_modules/**",
+      presets: ['@babel/env', '@babel/preset-react']
     }),
+    sourcemaps(),
     resolve(),
     cleanup(),
     commonjs()
